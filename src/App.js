2348d9a4ea266b5b07f7381013c1cfb4d1392a5d
Monish Narwani <mnarwani@avahitech.com>
2023-04-27 12:00:39 +0530

    WIP

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function App () {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>Welcome to AWS CodePipeline world!</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
